755a5bf4c4877c0c6149b0369a93c47f768e22fb
Stéphane Bressani <s.bressani@bluewin.ch>
2019-10-08 00:28:38 +0200

    it's compile but it's dirty, and python don't work https://github.com/srplab/starcore_for_flutter/issues/4#issuecomment-539224486

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
+Future<void> testCallPython() async {
+    print('1');
+    StarCoreFactory starcore = await Starflut.getFactory();
+    print('2');
+    StarServiceClass Service = await starcore.initSimple("test", "123", 0, 0, new List<String>());
+    print('3');
+    await starcore.regMsgCallBackP(
+        (int serviceGroupID, int uMsg, Object wParam, Object lParam) async{
+      print("$serviceGroupID  $uMsg   $wParam   $lParam");
+      return null;
+    });
+    print('4');
+    StarSrvGroupClass SrvGroup = await Service["_ServiceGroup"];
+
+    /*---script python--*/
+    bool isAndroid = await Starflut.isAndroid();
+    if( isAndroid == true ){
+      await Starflut.copyFileFromAssets("testpy.py", "flutter_assets/starfiles","flutter_assets/starfiles");
+      await Starflut.copyFileFromAssets("python3.6.zip", "flutter_assets/starfiles",null);  //desRelatePath must be null 
+      await Starflut.copyFileFromAssets("zlib.cpython-36m.so", null,null);
+      await Starflut.copyFileFromAssets("unicodedata.cpython-36m.so", null,null);
+      await Starflut.loadLibrary("libpython3.6m.so");
+    }
+
+    String docPath = await Starflut.getDocumentPath();
+    print("docPath = $docPath");
+
+    String resPath = await Starflut.getResourcePath();
+    print("resPath = $resPath");
+
+    dynamic rr1 = await SrvGroup.initRaw("python36", Service);
+
+    print("initRaw = $rr1");
+		var Result = await SrvGroup.loadRawModule("python", "", resPath + "/flutter_assets/starfiles/" + "testpy.py", false);
+    print("loadRawModule = $Result");
+
+		dynamic python = await Service.importRawContext("python", "", false, "");
+    print("python = "+ await python.getString());
+
+		StarObjectClass retobj = await python.call("tt", ["hello ", "world"]);
+    print(await retobj[0]);
+    print(await retobj[1]);
+
+    print(await python["g1"]);
+        
+    StarObjectClass yy = await python.call("yy", ["hello ", "world", 123]);
+    print(await yy.call("__len__",[]));
+
+    StarObjectClass multiply = await Service.importRawContext("python", "Multiply", true, "");
+    StarObjectClass multiply_inst = await multiply.newObject(["", "", 33, 44]);
+    print(await multiply_inst.getString());
+
+    print(await multiply_inst.call("multiply", [11, 22]));
+
+    await SrvGroup.clearService();
+		await starcore.moduleExit();
+  }
+
+
+
+
+
   CalcZodiac _calcZodiac;
   List<ZodiacSvgReturn> _zodiacSvg;
   AscReturn _ascReturn;
@@ -75,6 +139,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      testCallPython();
     });
   }
 
