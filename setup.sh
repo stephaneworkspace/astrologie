404cf0ee0b7873be83ecb085c60cd8e56cbc9ce4
stephanebressani <s.bressani@bluewin.ch>
2019-10-15 00:37:03 +0200

    interface between private python, public python and public dart

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ cd flatlib
 python3 setup.py install
 cd ..
 python3 setup.py install
+rm -rf astro_py_texte
+# Repository private for text on french, this library is not required, text comming from book, not my texts
+git clone https://github.com/stephaneworkspace/astro_py_text.git
+cd astro_py_text
+python3 setup.py install
