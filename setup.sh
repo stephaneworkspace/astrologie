92ec95c9232e71583b08dddecf7114a90561cb47
stephanebressani <s.bressani@bluewin.ch>
2019-10-15 04:09:15 +0200

    new standard

--- setup.sh
+++ setup.sh
@@ -9,6 +9,4 @@ cd ..
 python3 setup.py install
 rm -rf astro_py_texte
 # Repository private for text on french, this library is not required, text comming from book, not my texts
-git clone https://github.com/stephaneworkspace/astro_py_text.git
-cd astro_py_text
-python3 setup.py install
+git clone https://github.com/stephaneworkspace/astro_py_text.git assets
