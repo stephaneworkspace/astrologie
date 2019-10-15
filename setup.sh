a4632aef570174200f630249e81b607f8f0d315f
stephanebressani <s.bressani@bluewin.ch>
2019-10-15 14:56:18 +0200

    import svg from astro_py_text (private repostiory)

--- setup.sh
+++ setup.sh
@@ -10,3 +10,7 @@ python3 setup.py install
 rm -rf astro_py_texte
 # Repository private for text on french, this library is not required, text comming from book, not my texts
 git clone https://github.com/stephaneworkspace/astro_py_text.git assets
+cd assets
+cd svg
+rm -rf ../../../../assets/svg/astro_py_text
+mv astro_py_text ../../../../assets/svg
